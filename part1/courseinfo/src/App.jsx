34496f239f03cccf6c8d03debd98725cf72190da
Siiri Kemppainen <siiri.it.kemppainen@gmail.com>
2026-04-27 14:11:05 +0300

    Task 1.4 initial commit

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,42 +18,32 @@ const Part = ({ name, exercise }) => {
   )
 }
 
-const Content = ({names, exercises}) => {
-  if (names.length === exercises.length) {
-    // Creating the loop to render both list contents as long as they are
-    /**const paragraphs = [];
-    for (let i = 0; i < parts.length; i++) {
-        paragraphs.push(
-        <p>
-          {parts[i]} {exercises[i]}
-        </p>
-        );
-    }
-    return (
-      <div>
-        {paragraphs}
-      </div>
-    )**/
-   // This here is the "bad" way of doing this as well, but done as by the example:
-   return (
-    <div>
-      <Part name={names[0]} exercise={exercises[0]}/>
-      <Part name={names[1]} exercise={exercises[1]}/>
-      <Part name={names[2]} exercise={exercises[2]}/>
-    </div>
-   )
-  } else {
-    return (
-      <div>
-        <p>
-          Something went wrong, not equal array sizes
-        </p>
-      </div>
-    )
+const Content = ({ parts }) => {
+  // Creating the loop to render both list contents as long as they are
+  /**const paragraphs = [];
+  for (let i = 0; i < parts.length; i++) {
+      paragraphs.push(
+      <p>
+        {parts[i]} {exercises[i]}
+      </p>
+      );
   }
+  return (
+    <div>
+      {paragraphs}
+    </div>
+  )**/
+  // This here is the "bad" way of doing this as well, but done as by the example:
+  return (
+  <div>
+    <Part name={parts[0].name} exercise={parts[0].exercises}/>
+    <Part name={parts[1].name} exercise={parts[1].exercises}/>
+    <Part name={parts[2].name} exercise={parts[2].exercises}/>
+  </div>
+  )
 }
 
-const Total = ({ exercises }) => {
+const Total = ({ parts }) => {
   /**return (
     <div>
       <p>Number of exercises {exercises.reduce((sum, value) => sum + value, 0)}</p>
@@ -62,31 +52,33 @@ const Total = ({ exercises }) => {
  // This here is the "bad" way of doing this as well, but done as by the example:
  return (
   <div>
-    <p>Number of exercises {exercises[0] + exercises[1] + exercises[2]}</p>
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
   </div>
  )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content names={[part1.name, part2.name, part3.name]} exercises={[part1.exercises, part2.exercises, part3.exercises]}/>
-      <Total  exercises={[part1.exercises, part2.exercises, part3.exercises]}/>
+      <Content parts={parts}/>
+      <Total  parts={parts}/>
     </div>
   )
 }
