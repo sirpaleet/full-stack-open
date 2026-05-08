bb0430f4b15f53d2464f7f0f780ca5ba3a1fde56
Siiri Kemppainen <siiri.it.kemppainen@gmail.com>
2026-05-08 16:07:07 +0300

    Task 1.8 initial solution

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,20 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.good + props.neutral + props.bad}</p>
+      <p>average {props.countAverage()}</p>
+      <p>positive {props.countPositive()}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -26,13 +40,7 @@ const App = () => {
       <Button text="good" onClick={() => setGood(good + 1)} />
       <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
       <Button text="bad" onClick={() => setBad(bad + 1)} />
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {countAverage()}</p>
-      <p>positive {countPositive()}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} countAverage={countAverage} countPositive={countPositive} />
     </div>
   )
 }
