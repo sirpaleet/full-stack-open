4030611b68e62cecf0a8b06eaa484b2ad130dc90
Siiri Kemppainen <siiri.it.kemppainen@gmail.com>
2026-05-08 16:54:40 +0300

    Task 1.9 initial solution

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,6 +7,14 @@ const Button = (props) => (
 )
 
 const Statistics = (props) => {
+  if ((props.good + props.neutral + props.bad) === 0 ) {
+    return (
+    <div>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+    </div>
+    )
+  }
   return (
     <div>
       <h1>statistics</h1>
