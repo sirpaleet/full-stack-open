43e86d4e975fa22bc4c18a2be6908970f2e96a2c
Siiri Kemppainen <siiri.it.kemppainen@gmail.com>
2026-05-15 20:56:25 +0300

    Task 2.9 initial solution

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'arto Hellas', number: '1234567' },
+    { name: 'Hellas', number: '040' }
+  ]) 
+  const [shownPersons, setShownPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'arto Hellas', number: '1234567' },
+    { name: 'Hellas', number: '040' }
   ]) 
   const [newName, setNewName] = useState('') // so this is kind of treated as mandatory (is a key)
   const [newNumber, setNewNumber] = useState('') // while this is not
+  const [search, setSearch] = useState('') // while this is not
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -38,16 +46,23 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    setShownPersons(persons.filter((p) => p.name.toLowerCase().includes(event.target.value.toLowerCase())))
+    setSearch(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={search} onChange={handleSearchChange}/></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => <p key={p.name}>{p.name} {p.number}</p>)}
+      {shownPersons.map(p => <p key={p.name}>{p.name} {p.number}</p>)}
     </div>
   )
 }
